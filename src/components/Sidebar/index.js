98e896724ec87c1b911dc9f2e7d034530b0bdd06
aavetisyanit <aavetisyanit@gmail.com>
2022-04-07 17:02:33 -0500

    Add linkedin and github links to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -32,6 +33,24 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/artur--avetisyan/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/aavetisyanIT"
+        >
+          <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
