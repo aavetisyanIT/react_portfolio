6480f7c774f25b0e9c308ee1b0d3cae67f0d8d45
aavetisyanit <aavetisyanit@gmail.com>
2022-04-07 16:14:07 -0500

    Add routes to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +11,27 @@ const Sidebar = () => (
       <img src={LogoS} alt="logo" />
       <img class="sub-logo" src={LogoSubtitle} alt="avetisyan" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        className="about-link"
+        activeclassname="active"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        className="contact-link"
+        activeclassname="active"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 )
 
