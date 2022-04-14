ff210db29e7b8314bc7b1c27d05e59404b58b123
aavetisyanit <aavetisyanit@gmail.com>
2022-04-14 16:38:11 -0500

    Create cube spinner and animate it

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faJira,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = React.useState('text-animate')
@@ -35,6 +44,28 @@ const About = () => {
           person, father of a beautiful daughter, a sports fanatic, photography
           enthusiast, and tech-obsessed!!!
         </p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faJira} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
       </div>
     </div>
   )
